bf052d3e88d5b836c199af532b7fa5fbac6b3cc5
Mehmet Umut Ayan <106177950+mehmetumutayann@users.noreply.github.com>
2023-02-14 16:01:58 +0300

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,48 +1,53 @@
 #Parent Class
 
-class User():
-    def __init__(self,name,age,gender):
-        self.name = name
-        self.age = age
-        self.gender = gender
-
-    def show_details(self):
-        print("Kişisel Bilgiler")
-        print("")
-        print(f"Ad: {self.name}")
-        print(f"Yaş: {self.age}")
-        print(f"Cinsiyet: {self.gender}")
+class Person():
+    def __init__(self,ad,yas,cinsiyet):
+        self.ad = ad
+        self.yas = yas
+        self.cinsiyet = cinsiyet
+    
+    def detaylariGoster(self):
+        print("------------------")
+        print("-Kişisel Bilgiler-")
+        print("------------------")
+        print("Ad:",self.ad)
+        print("Yaş:",self.yas)
+        print("Cinsiyet:",self.cinsiyet)
+        print("----------------")
 
 #Child Class
 
-class Bank(User):
-    def __init__(self, name, age, gender):
-        super().__init__(name, age, gender),
-        self.balance = 0
-    
-    def deposit(self, amount):
-        self.amount = amount
-        self.balance = self.balance + amount
-        print("Hesap bakiyesi güncellendi: ", self.balance,"TL")
-    
-    def withdraw(self,amount):
-        self.amount = amount
-        if self.amount > self.balance:
-            print("Yetersiz Bakiye | Hesabınızda: ",self.balance,"TL bulunmaktadır.")
+class Bank(Person):
+    def __init__(self, ad, yas, cinsiyet):
+        super().__init__(ad, yas, cinsiyet) 
+        self.bakiye = 0
+
+    def paraYatir(self,miktar):
+        self.miktar = miktar
+        self.bakiye = self.bakiye + miktar
+        print(self.miktar,"TL para yatırıldı. | Hesabınızdaki bakiye:",self.bakiye,"TL olarak güncellendi.")
+
+    def paraCek(self,miktar):
+        self.miktar = miktar
+        if self.miktar > self.bakiye:
+            print("Yetersiz bakiye. | Hesabınızda",self.bakiye,"TL bulunmaktadır.")
         else:
-            self.balance = self.balance - self.amount
-            print("Hesabınızdan",self.amount,"TL çekildi | Kalan bakiye: ",self.balance,"TL")
+            self.bakiye = self.bakiye - self.miktar
+            print("Hesabınızdan",self.miktar,"TL çekildi. | Kalan bakiye:",self.bakiye)
 
-    def bakiyeGoruntule(self):
-        print("-----------")
-        self.show_details()
-        print("Hesabınızda: ",self.balance, "TL bulunmaktadır.")
-        print("-----------")
+    def bakiyeSor(self):
+        self.detaylariGoster()
+        print(f"Sayın {self.ad}. Hesabınızda:",self.bakiye,"TL bulunmaktadır.")
 
 
 
 
-mehmet = Bank("Mehmet Umut",15,"Erkek")
-mehmet.deposit(400)
-mehmet.deposit(240)
-mehmet.bakiyeGoruntule()
+
+
+
+
+
+MxAs = Bank("Mehmet", 15, "Erkek")
+MxAs.paraYatir(300)
+MxAs.paraYatir(422)
+MxAs.bakiyeSor()
